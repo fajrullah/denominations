c49499a7cf79b4822c8c7b0bc982c4eb9d040ae6
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-07-16 16:21:06 +0700

    s

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,39 @@
-const env = process.env.NODE_ENV
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+// React v.16 uses some newer JS functionality, so to ensure everything
+// works across all browsers, we're adding babel-polyfill here.
+require('babel-polyfill');
+
 module.exports = {
+  entry: [
+    './src/index'
+  ],
   module: {
-    rules: [
-              {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                  loader: 'babel-loader',
-                }
-              }
-            ]
+    loaders: [
+      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve('./'),
+      path.resolve('./node_modules'),
+    ],
+    extensions: ['.js','.scss'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
   ]
 };
